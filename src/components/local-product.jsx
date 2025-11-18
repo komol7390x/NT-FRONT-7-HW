3140384ee5e7a5c1ec8eb28150f753c1b9a47e71
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-18 23:00:25 +0500

    add

--- src/components/local-product.jsx
+++ src/components/local-product.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { formatter } from '../config/formater'
+import { useDispatch } from 'react-redux'
+import { decrementProduct, deleteProduct, incrementProduct } from '../store/reducer/product-reducer'
 
 export const LocalProduct = (product) => {
+    const dispatch = useDispatch()
     return (
         <div>
             <div className='border border-gray-100 w-[266px] h-[400px] text-left rounded-2xl shadow-xs'>
@@ -13,9 +16,23 @@ export const LocalProduct = (product) => {
                     <p className='underline-offset-4 underline'>Price: {formatter(product.price)} so'm</p>
                 </div>
                 <div className='flex justify-center'>
-                    <button className='mt-4 px-5 py-1 rounded-[10px] bg-amber-100 active:bg-amber-300'>Add</button>
+                    <div className='flex gap-3 justify-center items-center'>
+                        {product.userCount < 2 ? (
+                            <button
+                                onClick={() => dispatch(deleteProduct({ id: product.id }))}
+                                className='mt-4 px-5 py-1 rounded-[10px] bg-red-300 active:bg-amber-300'
+                            >delete</button>) : (<button
+                                onClick={() => dispatch(decrementProduct({ id: product.id }))}
+                                className='mt-4 px-5 py-1 rounded-[10px] bg-blue-200 active:bg-amber-300'
+                            >-</button>)}
+
+                        <span className='mt-4 px-5 py-1 rounded-[10px] bg-green-100'>{product.userCount}</span>
+                        <button
+                            onClick={() => dispatch(incrementProduct({ id: product.id }))}
+                            className='mt-4 px-5 py-1 rounded-[10px] bg-blue-200 active:bg-amber-300'>+</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
--- src/page/cart.jsx
+++ src/page/cart.jsx
@@ -8,10 +8,10 @@ export const Cart = () => {
         totalPrice,
         productList } = useSelector((state) => state.product)
     return (
-        <div className='container'>
+        <div className='container mt-10'>
             <h1 className='text-4xl'>Total Price: {formatter(totalPrice)}</h1>
             <p className='text-3xl'>Count: {count}</p>
-            <div className='grid grid-cols-4 gap-5'>
+            <div className='grid grid-cols-4 gap-5 mt-5'>
                 {productList.map((item) => (
                     <div>
                         <LocalProduct key={item.id} {...item} />
