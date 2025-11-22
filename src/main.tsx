f027a6d57133661c77f51af3f6266472c9d30351
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-22 14:27:16 +0500

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const client = new QueryClient
 
 createRoot(document.getElementById('root')!).render(
-  <>
-    <App />
-  </>,
+  <QueryClientProvider client={client}>
+    <BrowserRouter>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </BrowserRouter>
+  </QueryClientProvider>,
 )
