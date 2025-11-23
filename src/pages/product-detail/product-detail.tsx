d7ebf38312e16dae7e5bace014eff35c55fe96b4
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-23 18:54:06 +0500

    add

--- src/pages/product-detail/product-detail.tsx
+++ src/pages/product-detail/product-detail.tsx
@@ -1,19 +1,23 @@
 import { useParams } from "react-router"
 import { useUser } from "./query/useUser"
+import { CreateUser } from "@/components/form/form"
 
 export const ProductDetail = () => {
     const { id } = useParams()
     const { data, isLoading, isError } = useUser(id as string)
+
     if (isError) {
         return <h1 className="text-3xl">User not found</h1>
     }
     return (
-        <div className="container">
+        <div className="container border rounded-[10px] p-5 my-5 hover:border-yellow-200">
             {isLoading ? <h2>Loading...</h2> : (
                 <div>
-                    <h3>{data?.name}</h3>
+                    <h3 className="text-4xl ">{data?.name}</h3>
                     <p>{data?.email}</p>
                     <p>{data?.username}</p>
+                    <CreateUser email={data?.email} username={data?.username} id={data?.id} name={data?.name} />
+
                 </div>
 
             )}
