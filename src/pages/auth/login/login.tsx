c01b0ce0e43eb05161f29fde8fdb64735227563b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-11-26 23:04:57 +0500

    add

--- src/pages/auth/login/login.tsx
+++ src/pages/auth/login/login.tsx
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { useLogin } from '../service/use-login'
+import { useLogin } from './service/use-login'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import Cookies from 'js-cookie'
@@ -7,8 +7,7 @@ import { toast } from 'sonner'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 import { FormControl, FormField, FormItem, FormLabel, FormMessage, Form } from '@/components/ui/form'
-
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@radix-ui/react-select'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { PasswordInput } from '@/components/ui/password-input'
 import { Spinner } from '@/components/ui/spinner'
 import { Input } from '@/components/ui/input'
@@ -16,7 +15,7 @@ import { Input } from '@/components/ui/input'
 const formSchema = z.object({
   username: z.string().min(2).max(128).trim(),
   password: z.string().trim(),
-  role: z.string().trim()
+  role: z.enum(["Admin", "Teacher"]),
 })
 export const Login = () => {
   const { mutate, isPending } = useLogin()
@@ -27,10 +26,9 @@ export const Login = () => {
     defaultValues: {
       username: '',
       password: '',
-      role: ''
+      role: 'Admin'
     }
   })
-  // const [role, setRole] = useState("Admin")
 
   const onSubmit = (data: z.infer<typeof formSchema>) => {
     mutate(data, {
@@ -49,41 +47,39 @@ export const Login = () => {
   }
 
   return (
-    <div className="fixed bg-blue-300 inset-0 flex items-center justify-center">
+    <div className="fixed bg-yellow-100 inset-0 flex items-center justify-center">
       <Link to={'/register'}><Button className="absolute right-5 top-5 cursor-pointer">Sign up</Button>
       </Link>
-      <div className="w-[500px] bg-white shadow rounded-lg p-6">
+      <div className="w-[500px] bg-white shadow-xl rounded-lg p-6">
 
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
             className="space-y-[30px]"
           >
-
             <FormField
               control={form.control}
               name="role"
               render={({ field }) => (
                 <FormItem>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Role" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="Admin">Admin</SelectItem>
-                        <SelectItem value="Teacher">Teacher</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
+                  <FormLabel>Role</FormLabel>
+                  <Select
+                    value={field.value}
+                    onValueChange={field.onChange}
+                    defaultValue={field.value} // ✅ Qo'shing
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Role" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Admin">Admin</SelectItem>
+                      <SelectItem value="Teacher">Teacher</SelectItem>
+                    </SelectContent>
+                  </Select>
                   <FormMessage />
                 </FormItem>
               )}
             />
-
 
             <FormField
               control={form.control}
@@ -99,6 +95,7 @@ export const Login = () => {
               )}
             />
 
+
             <FormField
               control={form.control}
               name="password"
@@ -106,7 +103,7 @@ export const Login = () => {
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <PasswordInput placeholder="1234" {...field} />
+                    <PasswordInput placeholder="password" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
---
+++ src/pages/auth/login/service/use-login.ts
@@ -0,0 +1,12 @@
+import { useMutation } from "@tanstack/react-query"
+import type { ILoginResponse, ILogin } from "../../types"
+import { request } from "@/config/request"
+
+export const useLogin = () => {
+    return useMutation({
+        mutationFn: (data: ILogin) =>
+            request.post<ILoginResponse>('/auth/signin', data, {
+                withCredentials: true
+            }).then((res) => res.data)
+    })
+}
