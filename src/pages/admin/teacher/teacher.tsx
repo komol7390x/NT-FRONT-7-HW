cec1743a3e11fabc85563ac6d0c9e02c0224b1c1
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-12-07 22:54:54 +0500

    add

--- src/pages/admin/teacher/teacher.tsx
+++ src/pages/admin/teacher/teacher.tsx
@@ -7,6 +7,12 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } f
 import { TeacherFormWrapper } from "./components/teacher-form-wrapper";
 import { TeacherForm } from "./components/teacher-form";
 import { Button } from "@/components/ui/button";
+import type { ITeacher } from "../types/teacher";
+import { TeacherTable } from "./components/table";
+import { Skeleton } from "@/components/ui/skeleton";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import type { ColumnDef } from "@tanstack/react-table";
 
 export const Teacher = () => {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -21,12 +27,108 @@ export const Teacher = () => {
         close2();
     };
     const navigate = useNavigate();
+    const teachers: ITeacher[] = React.useMemo(() => {
+        if (!Array.isArray(data?.data)) return [];
+        return data.data.map((item, index) => ({
+            groups: item.groups?.length || 0,
+            id: item.id,
+            count: index + 1,
+            isActive: item.isActive ? "Active" : "Blocked",
+            name: item.name,
+            specification: item.specifications.map((item) => item.name).join(", "),
+            username: item.username,
+        }));
+    }, [data]);
+
+    const columns: ColumnDef<ITeacher>[] = [
+        {
+            accessorKey: "count",
+            header: "Count",
+        },
+        {
+            accessorKey: "name",
+            header: "Name",
+        },
+        {
+            accessorKey: "specification",
+            header: "Specification",
+        },
+        {
+            accessorKey: "username",
+            header: "Username",
+        },
+
+        {
+            accessorKey: "groups",
+            header: "Groups",
+            cell: ({ row }) => row.original.groups,
+        },
+
+        {
+            accessorKey: "isActive",
+            header: "Status",
+            cell: ({ row }) => {
+                const status = row.original.isActive;
+
+                return status === "Active" ? (
+                    <span className="text-green-600 font-semibold">Active</span>
+                ) : (
+                    <span className="text-red-500 font-semibold">Blocked</span>
+                );
+            },
+        },
+
+        {
+            id: "actions",
+            header: "Actions",
+            cell: ({ row }) => {
+                const teacher = row.original;
+
+                return (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem
+                                onClick={() => {
+                                    if (teacher.id) {
+                                        setEditID(teacher.id);
+                                        open2();
+                                    }
+                                }}
+                            >
+                                Edit
+                            </DropdownMenuItem>
+
+                            <DropdownMenuSeparator />
+
+                            <DropdownMenuItem
+                                onClick={() => navigate(`/app/admin/teacher/${teacher.id}`)}
+                            >
+                                View details
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                );
+            },
+        },
+    ];
+
 
     return (
         <div className="container">
             {isLoading || isFetching ? <Spinner /> : (
                 <div>
-                    <Dialog onOpenChange={closeEditModal} open={isOpen2}>
+                    <Dialog
+                        open={isOpen2}
+                        onOpenChange={(open) => {
+                            if (!open) closeEditModal();
+                        }}
+                    >
                         <DialogContent>
                             <DialogHeader>
                                 <DialogTitle>Teacher Edit</DialogTitle>
@@ -54,6 +156,26 @@ export const Teacher = () => {
                     <Button onClick={open} className="mb-5">
                         Create
                     </Button>
+                    <TeacherTable columns={columns} data={teachers} />
+                    {isFetching || isLoading ? (
+                        <Skeleton className="h-[30px] w-[300px]" />
+                    ) : (
+                        <div className="flex justify-end gap-3 mt-5">
+                            {buttons.map((_, index) => (
+                                <Button
+                                    onClick={() => {
+                                        setSearchParams({ page: `${index + 1}` });
+                                    }}
+                                    variant={
+                                        index + 1 === data?.currentPage ? "default" : "outline"
+                                    }
+                                >
+                                    {index + 1}
+                                </Button>
+                            ))}
+                        </div>
+                    )}
+
                 </div>
             )}
         </div>
